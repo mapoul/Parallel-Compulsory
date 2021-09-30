0c91cac23888809c972ed31dca9503c476d3be5e
Wezzydw <44640169+Wezzydw@users.noreply.github.com>
2021-09-30 10:37:32 +0200

    Update PrimeGenerator.cs

--- Parallel-Compulsory/Parallel-Compulsory/PrimeGenerator.cs
+++ Parallel-Compulsory/Parallel-Compulsory/PrimeGenerator.cs
@@ -8,12 +8,12 @@ namespace Parallel_Compulsory
 {
     public class PrimeGenerator
     {
-        List<long> GetPrimesSequential(long first, long last)
+        public List<long> GetPrimesSequential(long first, long last)
         {
             return null;
         }
 
-        List<long> GetPrimesParallel(long first, long last)
+        public List<long> GetPrimesParallel(long first, long last)
         {
             return null;
         }
