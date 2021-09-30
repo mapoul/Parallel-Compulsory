5e6f4f3385311902ac95b28c1598c1a3bca88ec4
Wezzydw <44640169+Wezzydw@users.noreply.github.com>
2021-09-30 10:37:18 +0200

    Create PrimeGenerator.cs

---
+++ Parallel-Compulsory/Parallel-Compulsory/PrimeGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallel_Compulsory
+{
+    public class PrimeGenerator
+    {
+        List<long> GetPrimesSequential(long first, long last)
+        {
+            return null;
+        }
+
+        List<long> GetPrimesParallel(long first, long last)
+        {
+            return null;
+        }
+    }
+}
