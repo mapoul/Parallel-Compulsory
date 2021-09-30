ee2a8c4f5722ba53b73d913575b379ca4286c376
mapoul <44639698+mapoul@users.noreply.github.com>
2021-09-30 10:02:34 +0200

    initial commit

---
+++ Parallel-Compulsory/Parallel-Compulsory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Parallel_Compulsory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
